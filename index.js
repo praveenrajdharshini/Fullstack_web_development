74727337b393621e476c8faa875f329a8cd74202
Swordship <monishravi508@gmail.com>
2024-10-03 17:40:12 +0530

    mongodb connection

--- index.js
+++ index.js
@@ -8,9 +8,7 @@ en.config();
 app.use(bodyparser.json())
 const PORT = process.env.PORT || 5000;  
 const encryp=process.env.encryp
-app.use('/student',newrouter)
-app.use('/teacher',newrouter)
-app.use('/attendee',newrouter)
+
 mongoose.connect("mongodb+srv://new_user11:newuser11@custmer.vocvmkz.mongodb.net/").then(()=>{
     console.log("mongodb conneted");
     app.listen(PORT,()=>{
